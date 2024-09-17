ba489e96db0b11270601f53630bf8e098d2b0bb4
wentingyun <2257448717@qq.com>
2024-09-17 15:32:43 +0800

    Firstcommit

---
+++ quickstart/src/main/java/quick/HelloSeverlet.java
@@ -0,0 +1,24 @@
+package quick;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/dtt")
+public class HelloSeverlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = resp.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" +"dtt" + "</h1>");
+        out.println("</body></html>");
+    }
+}
